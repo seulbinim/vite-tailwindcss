47d064e807aa5899dfb0f2be5f1570a5a7e399e4
슬비쌤 <seulbinim@gmail.com>
2025-04-10 14:43:31 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import path from "path";
 import fs from "fs";
+import tailwindcss from "@tailwindcss/vite";
 
 // findAllHtmlFiles 함수 정의 추가
 function findAllHtmlFiles(directory) {
